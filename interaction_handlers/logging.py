0b607891ba90081d79ed96e1abc7717867f84026
yuvraj108c <yuvraj108c@gmail.com>
2024-06-26 23:55:07 +0400

    trim traceback

--- interaction_handlers/logging.py
+++ interaction_handlers/logging.py
@@ -1,12 +1,14 @@
 from discord.ext import commands
 from constants import PROJECT_ART_SHARING_CHANNEL
+import textwrap
 
 
 async def handle_report_errors_interaction(bot: commands.Bot, traceback: str) -> None:
 
     project_art_sharing_channel = bot.get_channel(PROJECT_ART_SHARING_CHANNEL)
     # limit the traceback to 2000 characters
-    await project_art_sharing_channel.send(f"⚠️   ⚠️   ⚠️\n\n{traceback[:2000]}")
+    traceback_output = textwrap.shorten(traceback.format_exc(), width=1900)
+    await project_art_sharing_channel.send(f"⚠️   ⚠️   ⚠️\n\n{traceback_output}")
 
 
 async def handle_report_log_interaction(bot: commands.Bot, message: str) -> None:
